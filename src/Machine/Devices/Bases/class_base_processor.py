c22dc5762f3ebd6b40959148047ad33dc1e7c9c5
Kyle D. Ross <6475128+kyledross@users.noreply.github.com>
2024-04-17 12:35:12 -0400

    Moved processor phases to processor from base processor.
    Code and documentation cleanup.

--- src/Machine/Devices/Bases/class_base_processor.py
+++ src/Machine/Devices/Bases/class_base_processor.py
@@ -26,15 +26,3 @@ class BaseProcessor(BaseDevice):
         :raise NotImplementedError: If this method is called directly from the base class.
         """
         raise NotImplementedError
-
-    class Phases:
-        """
-        The Phases class represents the different phases a processor can be in during its operation.
-        It is a static class, meaning it does not need to be instantiated and its members can be accessed directly.
-        """
-        # todo: this seems to be processor-specific, so it might need to be moved to the processor class
-        NothingPending = 0  # The processor has nothing pending.
-        AwaitingInstruction = 1  # The processor is awaiting an instruction.
-        AwaitingFirstOperand = 2  # The processor is awaiting the first operand.
-        AwaitingSecondOperand = 3  # The processor is awaiting the second operand.
-        AwaitingResponse = 4  # The processor is awaiting a response.

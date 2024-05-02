a933400b5fc6ed33d1f0535e406cf246480b6cff
Kyle D. Ross <6475128+kyledross@users.noreply.github.com>
2024-05-02 15:58:09 -0400

    Initial implementation

--- src/Machine/Backplane/class_backplane.py
+++ src/Machine/Backplane/class_backplane.py
@@ -8,6 +8,7 @@ from Machine.Buses.class_control_bus import ControlBus
 from Machine.Buses.class_data_bus import DataBus
 from Machine.Buses.class_interrupt_bus import InterruptBus
 from Machine.Devices.Bases.class_base_device import BaseDevice
+from Machine import Devices
 
 
 class BackPlane:
@@ -45,6 +46,8 @@ class BackPlane:
     _controlBus: ControlBus = None
     _interruptBus: InterruptBus = None
 
+    _cataloged_devices = {}
+
     def __init__(self):
         """
         Constructs all the necessary attributes for the backplane.
@@ -76,6 +79,18 @@ class BackPlane:
         while running:
             for device in self._devices:
                 device.cycle(self._addressBus, self._dataBus, self._controlBus, self._interruptBus)
+                # if the device has set the response line, save the address it responded to
+                if self._controlBus.read_response():
+                    self._cataloged_devices[self._addressBus.get_address()] = device
+                # if the device is requesting an address, and we've cataloged the responding device for the address,
+                # cycle the responding device
+                if ((self._controlBus.get_read_request() or self._controlBus.get_write_request())
+                        and self._addressBus.get_address() in self._cataloged_devices):
+                    cataloged_device = self._cataloged_devices[self._addressBus.get_address()]
+                    cataloged_device.cycle(self._addressBus, self._dataBus, self._controlBus, self._interruptBus)
+                    # now cycle the current device again to give it a chance to respond to the read/write request
+                    device.cycle(self._addressBus, self._dataBus, self._controlBus, self._interruptBus)
+
                 if self._interruptBus.test_interrupt(Interrupts.halt):
                     running = False
                     break

92dadb85dae36b5b4df49b45092e3738ef4f7542
Kyle D. Ross <6475128+kyledross@users.noreply.github.com>
2024-05-18 19:31:52 -0400

    Added deprecation warnings to consoles v1 through v4.
    Refactored command-line parsing.

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@
 # lylt
 
 import sys
+import warnings
 
 
 # This is the entry point into the emulator.
@@ -36,7 +37,7 @@ def start_machine():
         if len(sys.argv) > 1:
 
             from MachineConfiguration.class_machine_builder import MachineBuilder
-            builder = MachineBuilder(parse_command_line_arguments())
+            builder = MachineBuilder(parse_command_line())
             builder.build_machine().run()
         else:
             show_help()
@@ -58,9 +59,9 @@ def check_required_parameters(device: str, parameters, keys):
             exit(1)
 
 
-def parse_command_line_arguments():
+def parse_command_line():
     """Parses the command line arguments and returns a list of device groups.  Each device group is a dictionary"""
-    device_groups = []
+    devices = []
     import argparse
 
     # Create an argument parser
@@ -82,88 +83,39 @@ def parse_command_line_arguments():
     parser.add_argument('--consolev31', type=lambda x: x.split('='), nargs='+')
     parser.add_argument("--compiler", type=lambda x: x.split('='), nargs='+')
     parser.add_argument("--display", type=lambda x: x.split('='), nargs='+')
-    # Parse the command-line arguments
+
     args = parser.parse_args()
     if args.help:
         show_help()
         exit()
 
-    if args.processor:
-        device_groups.append({'device_name': 'processor', 'options': ''})
-    if args.processor_with_cache:
-        device_groups.append({'device_name': 'processor_with_cache', 'options': ''})
-    if args.debugger:
-        device_groups.append({'device_name': 'debugger'})
-    if args.ram:
-        ram_args = dict(args.ram)
-        address = ram_args.get("address")
-        size = ram_args.get("size")
-        check_required_parameters("RAM", ram_args, ["address", "size"])
-        device_groups.append({'device_name': 'ram', 'address': address, 'size': size})
-    if args.console:
-        console_args = dict(args.console)
-        address = console_args.get("address")
-        interrupt = console_args.get("interrupt")
-        check_required_parameters("Console", console_args, ["address", "interrupt"])
-        device_groups.append({'device_name': 'console', 'address': address, 'interrupt': interrupt})
+    # build the devices list to pass to the machine builder
+    add_processor(args, devices)
+    add_processor_with_cache(args, devices)
+    add_debugger(args, devices)
+    add_ram(args, devices)
+    add_console_v1(args, devices)
+    add_console_v2(args, devices)
+    add_console_v3(args, devices)
+    add_console_v31(args, devices)
+    add_console_v4(args, devices)
+    add_console_v5(args, devices)
+    add_addressable_display(args, devices)
+    add_compiler(args, devices)
+    return devices
 
-    if args.consolev2:
-        console_args = dict(args.consolev2)
-        address = console_args.get("address")
-        interrupt = console_args.get("interrupt")
-        # noinspection SpellCheckingInspection
-        check_required_parameters("Consolev2", console_args, ["address", "interrupt"])
-        # noinspection SpellCheckingInspection
-        device_groups.append({'device_name': 'consolev2', 'address': address, 'interrupt': interrupt})
 
-    if args.consolev3:
-        console_args = dict(args.consolev3)
-        address = console_args.get("address")
-        interrupt = console_args.get("interrupt")
-        width = console_args.get("width")
-        height = console_args.get("height")
-        # noinspection SpellCheckingInspection
-        check_required_parameters("Consolev3", console_args, ["address", "interrupt", "width", "height"])
-        # noinspection SpellCheckingInspection
-        device_groups.append(
-            {'device_name': 'consolev3', 'address': address, 'interrupt': interrupt, 'width': width, 'height': height})
+def add_compiler(args, devices):
+    if args.compiler:
+        compiler_args = dict(args.compiler)
+        address = compiler_args.get("address")
+        program = compiler_args.get("program")
+        size = compiler_args.get("size")
+        check_required_parameters("Compiler", compiler_args, ["address", "program", "size"])
+        devices.append({'device_name': 'compiler', 'address': address, 'program': program, 'size': size})
 
-    if args.consolev31:
-        console_args = dict(args.consolev31)
-        address = console_args.get("address")
-        interrupt = console_args.get("interrupt")
-        width = console_args.get("width")
-        height = console_args.get("height")
-        # noinspection SpellCheckingInspection
-        check_required_parameters("Consolev31", console_args, ["address", "interrupt", "width", "height"])
-        # noinspection SpellCheckingInspection
-        device_groups.append(
-            {'device_name': 'consolev31', 'address': address, 'interrupt': interrupt, 'width': width, 'height': height})
 
-    if args.consolev4:
-        console_args = dict(args.consolev4)
-        address = console_args.get("address")
-        interrupt = console_args.get("interrupt")
-        width = console_args.get("width")
-        height = console_args.get("height")
-        # noinspection SpellCheckingInspection
-        check_required_parameters("Consolev4", console_args, ["address", "interrupt", "width", "height"])
-        # noinspection SpellCheckingInspection
-        device_groups.append(
-            {'device_name': 'consolev4', 'address': address, 'interrupt': interrupt, 'width': width, 'height': height})
-
-    if args.consolev5:
-        console_args = dict(args.consolev5)
-        address = console_args.get("address")
-        interrupt = console_args.get("interrupt")
-        width = console_args.get("width")
-        height = console_args.get("height")
-        # noinspection SpellCheckingInspection
-        check_required_parameters("Consolev5", console_args, ["address", "interrupt", "width", "height"])
-        # noinspection SpellCheckingInspection
-        device_groups.append(
-            {'device_name': 'consolev5', 'address': address, 'interrupt': interrupt, 'width': width, 'height': height})
-
+def add_addressable_display(args, devices):
     if args.display:
         display_args = dict(args.display)
         address = display_args.get("address")
@@ -174,17 +126,112 @@ def parse_command_line_arguments():
         if "height" in display_args:
             height = display_args.get("height")
         check_required_parameters("Display", display_args, ["address"])
-        device_groups.append({'device_name': 'display', 'address': address, 'width': width, 'height': height})
+        devices.append({'device_name': 'display', 'address': address, 'width': width, 'height': height})
 
-    if args.compiler:
-        compiler_args = dict(args.compiler)
-        address = compiler_args.get("address")
-        program = compiler_args.get("program")
-        size = compiler_args.get("size")
-        check_required_parameters("Compiler", compiler_args, ["address", "program", "size"])
-        device_groups.append({'device_name': 'compiler', 'address': address, 'program': program, 'size': size})
 
-    return device_groups
+def add_console_v5(args, devices):
+    if args.consolev5:
+        console_args = dict(args.consolev5)
+        address = console_args.get("address")
+        interrupt = console_args.get("interrupt")
+        width = console_args.get("width")
+        height = console_args.get("height")
+        # noinspection SpellCheckingInspection
+        check_required_parameters("Consolev5", console_args, ["address", "interrupt", "width", "height"])
+        # noinspection SpellCheckingInspection
+        devices.append(
+            {'device_name': 'consolev5', 'address': address, 'interrupt': interrupt, 'width': width, 'height': height})
+
+
+def add_console_v4(args, devices):
+    if args.consolev4:
+        warnings.warn("Warning: The consolev4 device is deprecated.  Use --consolev5 instead.")
+        console_args = dict(args.consolev4)
+        address = console_args.get("address")
+        interrupt = console_args.get("interrupt")
+        width = console_args.get("width")
+        height = console_args.get("height")
+        # noinspection SpellCheckingInspection
+        check_required_parameters("Consolev4", console_args, ["address", "interrupt", "width", "height"])
+        # noinspection SpellCheckingInspection
+        devices.append(
+            {'device_name': 'consolev4', 'address': address, 'interrupt': interrupt, 'width': width, 'height': height})
+
+
+def add_console_v31(args, devices):
+    if args.consolev31:
+        warnings.warn("Warning: The consolev31 device is deprecated.  Use --consolev5 instead.")
+        console_args = dict(args.consolev31)
+        address = console_args.get("address")
+        interrupt = console_args.get("interrupt")
+        width = console_args.get("width")
+        height = console_args.get("height")
+        # noinspection SpellCheckingInspection
+        check_required_parameters("Consolev31", console_args, ["address", "interrupt", "width", "height"])
+        # noinspection SpellCheckingInspection
+        devices.append(
+            {'device_name': 'consolev31', 'address': address, 'interrupt': interrupt, 'width': width, 'height': height})
+
+
+def add_console_v3(args, devices):
+    if args.consolev3:
+        warnings.warn("Warning: The consolev3 device is deprecated.  Use --consolev5 instead.")
+        console_args = dict(args.consolev3)
+        address = console_args.get("address")
+        interrupt = console_args.get("interrupt")
+        width = console_args.get("width")
+        height = console_args.get("height")
+        # noinspection SpellCheckingInspection
+        check_required_parameters("Consolev3", console_args, ["address", "interrupt", "width", "height"])
+        # noinspection SpellCheckingInspection
+        devices.append(
+            {'device_name': 'consolev3', 'address': address, 'interrupt': interrupt, 'width': width, 'height': height})
+
+
+def add_console_v2(args, devices):
+    if args.consolev2:
+        warnings.warn("Warning: The consolev2 device is deprecated.  Use --consolev5 instead.")
+        console_args = dict(args.consolev2)
+        address = console_args.get("address")
+        interrupt = console_args.get("interrupt")
+        # noinspection SpellCheckingInspection
+        check_required_parameters("Consolev2", console_args, ["address", "interrupt"])
+        # noinspection SpellCheckingInspection
+        devices.append({'device_name': 'consolev2', 'address': address, 'interrupt': interrupt})
+
+
+def add_console_v1(args, devices):
+    if args.console:
+        warnings.warn("Warning: The console device is deprecated.  Use --consolev5 instead.")
+        console_args = dict(args.console)
+        address = console_args.get("address")
+        interrupt = console_args.get("interrupt")
+        check_required_parameters("Console", console_args, ["address", "interrupt"])
+        devices.append({'device_name': 'console', 'address': address, 'interrupt': interrupt})
+
+
+def add_ram(args, devices):
+    if args.ram:
+        ram_args = dict(args.ram)
+        address = ram_args.get("address")
+        size = ram_args.get("size")
+        check_required_parameters("RAM", ram_args, ["address", "size"])
+        devices.append({'device_name': 'ram', 'address': address, 'size': size})
+
+
+def add_debugger(args, devices):
+    if args.debugger:
+        devices.append({'device_name': 'debugger'})
+
+
+def add_processor_with_cache(args, devices):
+    if args.processor_with_cache:
+        devices.append({'device_name': 'processor_with_cache', 'options': ''})
+
+
+def add_processor(args, devices):
+    if args.processor:
+        devices.append({'device_name': 'processor', 'options': ''})
 
 
 def show_help():

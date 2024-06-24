5a6e2f28316af7a4e052c0707554cb7d8a82b9dd
Kyle D. Ross <6475128+kyledross@users.noreply.github.com>
2024-06-24 09:18:34 -0400

    Updated help

--- src/main.py
+++ src/main.py
@@ -211,8 +211,7 @@ def show_help() -> None:
     print()
     print("--compiler")
     print("   Adds a RAM device to the backplane and compiles a Rubbish assembly language program into the")
-    print("   address space of the RAM.  The size of the RAM that is added is exactly the size of the compiled")
-    print("   program.")
+    print("   address space of the RAM.")
     print()
     print("   Syntax:")
     print("         --compiler address={starting address} size={size of ram address space} "

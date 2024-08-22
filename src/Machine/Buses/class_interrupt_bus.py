c6b7f0cf564a368cb41fcd77db8c85274c753bde
Kyle Ross <6475128+kyledross@users.noreply.github.com>
2024-08-22 19:00:15 -0400

    Refactor interrupt handling infrastructure
    Replaced bitwise interrupt handling with dictionary-based implementation. Removed the use of `IntFlag` and simplified interrupt testing and clearing methods. Updated documentation to reflect these changes and improve clarity.

--- src/Machine/Buses/class_interrupt_bus.py
+++ src/Machine/Buses/class_interrupt_bus.py
@@ -1,11 +1,4 @@
-import Constants.class_interrupts
 from Constants.class_interrupts import Interrupts
-
-# todo: the structure of interrupts really needs to change
-# this implementation tries to replicate bitwise interrupts, and that ends up being just wonky
-# in the real world, an interrupt is an interrupt.  interrupt 3 isn't interrupts 1 and 2... it's 3
-# so rather than be a bitmap, the interrupt bus needs to be a dictionary of bits with the key being the interrupt
-# number and the value being the state
 
 class InterruptBus:
     """
@@ -18,42 +11,44 @@ class InterruptBus:
         Constructor for the InterruptBus class.
         Initializes the interrupts on the bus to 0.
         """
-        self.__interruptBus = Interrupts.none
+        self.__activeInterrupts = {}
 
-    def interrupt_awaiting(self) -> Interrupts:
+    def interrupt_awaiting(self) -> int:
         """
         This method checks if any interrupts are set on the bus and, if so, returns the interrupt number to handle.
         Interrupts with lower numbers have priority.
         :return: The highest-priority interrupt number to handle, or none if no interrupts are set.
         """
-        if self.__interruptBus == Interrupts.none:
+        if len(self.__activeInterrupts) == 0:
             return Interrupts.none
-        for interruptBit in range(0, 32):
-            interrupt_number: Interrupts = 2 ** interruptBit
-            if self.test_interrupt(interrupt_number):
-                return interrupt_number
+        
+        highest_priority_interrupt = min(self.__activeInterrupts.keys())
+        return highest_priority_interrupt
 
-    def set_interrupt(self, value: Constants.class_interrupts):
+    def set_interrupt(self, value: int):
         """
         This method sets an interrupt on the bus.
         It uses bitwise OR to set the interrupt.
         :param value: The interrupt to set on the bus.
         """
-        self.__interruptBus = self.__interruptBus | value
+        self.__activeInterrupts[value] = True
 
-    def test_interrupt(self, interrupt_number: Constants.class_interrupts) -> bool:
+
+    def test_interrupt(self, interrupt_number: int) -> bool:
         """
         This method tests if a specific interrupt is set on the bus.
-        It uses bitwise AND to test the interrupt.
+        It checks the presence of the interrupt in the set.
         :param interrupt_number: The interrupt to test on the bus.
-        :return: True if the interrupt is set, False otherwise.
+        :return: True if the interrupt is present in the set, False otherwise.
         """
-        return self.__interruptBus & interrupt_number != Interrupts.none
+        return interrupt_number in self.__activeInterrupts
 
-    def clear_interrupt(self, interrupt_number: Constants.class_interrupts):
+
+    def clear_interrupt(self, interrupt_number: int):
         """
         This method clears a specific interrupt on the bus.
         It uses bitwise AND with bitwise NOT to clear the interrupt.
         :param interrupt_number: The interrupt to clear on the bus.
         """
-        self.__interruptBus = self.__interruptBus & ~interrupt_number
+        if interrupt_number in self.__activeInterrupts:
+            del self.__activeInterrupts[interrupt_number]

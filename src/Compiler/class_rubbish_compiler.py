e218944ceae5af0876ef7d99a7eef78ff2e90b5a
Kyle D. Ross <6475128+kyledross@users.noreply.github.com>
2025-02-06 00:54:15 -0500

    Revert "Re-added CMP compilation."
    This reverts commit 2fae1a0c20d5ff3fd7d456f818385d0b2ecb8e5d.

--- src/Compiler/class_rubbish_compiler.py
+++ src/Compiler/class_rubbish_compiler.py
@@ -55,6 +55,10 @@ class RubbishCompiler:
                         if instruction == "'" or instruction == "#":
                             # This is just a comment
                             pass
+                        elif instruction == "CMP":
+                            # compare is no longer necessary, as the processor will trigger an automatic compare
+                            # on every change of register 1 or 2
+                            pass
                         elif instruction == "NOP":
                             self.add_instruction(0, [], code, phase == 2)
 
@@ -65,7 +69,7 @@ class RubbishCompiler:
                             code.extend(ord(char) for char in data)
 
                         elif instruction in {"LR", "LRM", "LRR", "MRM", "ADD", "DIV", "MUL", "SUB", "JMP", "HALT",
-                                             "DEBUG", "RST", "CMP", "JE", "JNE", "JL", "JG", "PUSH", "POP", "CALL",
+                                             "DEBUG", "RST", "JE", "JNE", "JL", "JG", "PUSH", "POP", "CALL",
                                              "RTN", "SIV", "SLEEP", "WAKE", "PEEK", "OR", "AND", "XOR", "NOT", "INC", "DEC", "INT"}:
                             self.add_instruction(self.get_instruction_code(instruction), parameters, code, phase == 2)
 
